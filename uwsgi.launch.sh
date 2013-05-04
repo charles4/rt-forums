7d282e9278a03c67a62d5baa8a5e36ee673094e2
charles <charles4@email.arizona.edu>
2013-05-04 16:31:16 -0700

    adjust config

--- uwsgi.launch.sh
+++ uwsgi.launch.sh
@@ -1,1 +1,1 @@
-uwsgi --socket 127.0.0.1:3031 --wsgi-file forum.py --callable app --processes 1 --threads 2 --stats 127.0.0.1:9191
+uwsgi --socket 127.0.0.1:3031 --wsgi-file forum.py --callable app --processes 4 --threads 2 --stats 127.0.0.1:9191
