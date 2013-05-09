8d1aa1584e00b92ed623448a2c3bffdf663e53e4
charles <charles4@email.arizona.edu>
2013-05-09 14:49:21 -0700

    a

--- uwsgi.launch.sh
+++ uwsgi.launch.sh
@@ -1,1 +1,1 @@
-uwsgi --master --socket :3031 --wsgi-file forum.py --callable app --processes 4 --threads 2 --stats 127.0.0.1:9191 --die-on-term --daeomonize /home/charles/logs/uwsgi.log
+uwsgi --master --socket :3031 --wsgi-file forum.py --callable app --processes 4 --threads 2 --stats 127.0.0.1:9191 --die-on-term --daemonize /home/charles/logs/uwsgi.log
